ca0046106fbbabd0184769b5581a4650026d9574
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2015-12-05 19:49:15 -0300

    Funcao buscarLugar adicionada no Header

--- listao.h
+++ listao.h
@@ -1,25 +1,25 @@
 /* Tipo que descreve as caracteristicas da Flecha */
 typedef struct flecha_st
 {
-        int de;
-            int para;
-                int tk;
-                    struct flecha_st * prox;
+    int de;
+    int para;
+    int tk;
+    struct flecha_st * prox;
 }flecha;
 
 /* Tipo que descreve as caracteristicas dos Lugares */
 typedef struct lugar_st
 {
-        int qtd;
-            struct lugar_st * prox;
+    int qtd;
+    struct lugar_st * prox;
 }lugar;
 
 /* Descricao da rede de Petri sem as transicoes */ 
 typedef struct petri_st
 {
-        lugar l;
-            flecha lt;
-                flecha tl;
+    lugar l;
+    flecha lt;
+    flecha tl;
 }petri_t;
 
-
+lugar *buscarLugar(lugar *cabeca, int x);
