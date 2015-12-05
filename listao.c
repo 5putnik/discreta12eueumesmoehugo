7a49f0ebc6ac2dbb4cd3d0c0cba2e2c6a755c5db
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2015-12-05 20:04:27 -0300

    Funcao buscarLugar adicionada no arquivo .c

--- listao.c
+++ listao.c
@@ -1,1 +1,11 @@
-/* Arquivo em branco */
+lugar *buscarLugar(lugar *cabeca, int x)
+{
+    lugar *pl = cabeca;
+    while(pl != NULL)
+    {
+        if(pl -> qtd == x)
+            return pl;
+        pl = pl -> prox;
+    }
+    return NULL;
+}
