e11ec3422155b7eb5961a8fc84151d0cef99378c
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2015-12-05 19:21:46 -0300

    Arquivo .h da biblioteca listao criado, structs da main transferidas

---
+++ listao.h
@@ -0,0 +1,25 @@
+/* Tipo que descreve as caracteristicas da Flecha */
+typedef struct flecha_st
+{
+        int de;
+            int para;
+                int tk;
+                    struct flecha_st * prox;
+}flecha;
+
+/* Tipo que descreve as caracteristicas dos Lugares */
+typedef struct lugar_st
+{
+        int qtd;
+            struct lugar_st * prox;
+}lugar;
+
+/* Descricao da rede de Petri sem as transicoes */ 
+typedef struct petri_st
+{
+        lugar l;
+            flecha lt;
+                flecha tl;
+}petri_t;
+
+
