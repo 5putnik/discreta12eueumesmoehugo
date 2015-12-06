2deb0e54779acef81cad03862966e5e3bab9abe1
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2015-12-06 08:37:01 -0300

    Funcao inserirLugar adicionada ao Arquivo Fonte .c

--- listao.c
+++ listao.c
@@ -9,3 +9,24 @@ lugar *buscarLugar(lugar *cabeca, int x)
     }
     return NULL;
 }
+
+void inserirLugar(lugar **cabeca, int x)
+{
+    lugar *pl = cabeca;
+    lugar *plant = NULL;
+    
+    while(pl != NULL)
+    {
+        plant -> pl;
+        pl = pl -> prox;
+    }
+    
+    pl = malloc(sizeof(lugar));
+    pl -> qtd = x;
+    pl -> prox = NULL;
+    
+    if(plant != NULL)
+        plant -> prox = pl;
+    else
+        *cabeca = pl;
+}
